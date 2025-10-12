73807ad1dd03fd391fd200b7ee69e1550520b72b
Mert Özen <Mert zen>
2025-10-12 14:51:32 +0300

    7.ders notlari

---
+++ NetCoreWebApiDemo/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreWebApiDemo.Models;
+
+namespace NetCoreWebApiDemo
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+                
+        }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ NetCoreWebApiDemo/Controllers/ProductController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreWebApiDemo.Models;
+using NetCoreWebApiDemo.Services;
+
+namespace NetCoreWebApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+       private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService=productService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                return Ok(_productService.GetAll());
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }            
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                var product= _productService.GetById(id);
+                return product==null?NotFound():Ok(product);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Add(Product product)
+        {
+            try
+            {
+                _productService.Add(product);
+                return CreatedAtAction(nameof(GetById), new {id=product.Id},product);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id,Product product)
+        {
+            try
+            {
+                _productService.Update(id,product);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                if(ex.Message=="Ürün bulunamadı")
+                {
+                    return NotFound();
+                }
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                _productService.Delete(id);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message == "Ürün bulunamadı")
+                {
+                    return NotFound();
+                }
+                return BadRequest();
+            }
+        }
+    }
+}
