a8f64405228d3e7fa168746b2b29c418a4bf0f3d
Mert Özen <Mert zen>
2026-02-11 21:17:41 +0300

    22.ders notlari

---
+++ NetCoreWebApiDemo/Controllers/VehicleController.cs
@@ -0,0 +1,24 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreWebApiDemo.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
+    [Route("api/vehicle")]
+    [Route("api/v{version:apiVersion}/vehicle")]
+    public class VehicleController : ControllerBase
+    {
+        [HttpGet]
+        [MapToApiVersion("1.0")]
+        public IActionResult GetV1()
+        => Ok(new[] { new { id = 1, plate = "34 MRT 95" } });
+
+        [HttpGet]
+        [MapToApiVersion("2.0")]
+        public IActionResult GetV2()
+            => Ok(new[] { new { id = 1, plate = "34 MRT 95", driver = "Mert Özen"} });
+    }
+}
